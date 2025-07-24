1436a41e6a58f4908066a56601233795a8a399b5
nuclearpolygon <45406953+nuclearpolygon@users.noreply.github.com>
2025-07-24 04:05:39 +0300

    more log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,24 @@
+"set -e 
+ log() { echo '>> [remote]' \$@ 
+ } 
+ cleanup() { log 'Removing workspace...'
+ rm -rf \"\$HOME/.workspace\" 
+ } 
+ log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\"
+ log DOCKER_COMPOSE_PREFIX \"$DOCKER_COMPOSE_PREFIX\"
+ log HOME \"$HOME\"
+ log 'Creating workspace directory...' 
+ mkdir -p \"\$HOME/.workspace\" 
+ trap cleanup EXIT 
+ log 'Unpacking workspace...' 
+ tar -C \"\$HOME/.workspace\" -xjv 
+ log 'Launching docker compose...' 
+ log 'Current dir: \"\$HOME/.workspace\"'
+  cd \"\$HOME/.workspace\" 
+ log 'Pull images...' 
+ docker compose -f \"$DOCKER_COMPOSE_FILENAME\" pull 
+ docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
+
 #!/usb/bin/env bash
 set -e
 
@@ -20,9 +41,9 @@ tar cjvf /tmp/workspace.tar.bz2 --exclude .git .
 log "Launching ssh agent."
 eval `ssh-agent -s`
 
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace\" -xjv ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Launching docker compose...' ; cd \"\$HOME/.workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
+remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace\" -xjv ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Launching docker compose...' ; echo current dir: \"\$HOME/.workspace\" ; cd \"\$HOME/.workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
 if $PULL; then
-  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace\" -xjv ; log 'Launching docker compose...' ; cd \"\$HOME/.workspace\" ; log 'Pull images...' ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" pull ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
+  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace\" -xjv ; log 'Launching docker compose...' ; echo current dir: \"\$HOME/.workspace\" ;  cd \"\$HOME/.workspace\" ; log 'Pull images...' ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" pull ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
 fi
 if $USE_DOCKER_STACK ; then
   remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace/$DOCKER_COMPOSE_PREFIX\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace/$DOCKER_COMPOSE_PREFIX\" -xjv ; log 'Launching docker stack deploy...' ; cd \"\$HOME/.workspace/$DOCKER_COMPOSE_PREFIX\" ; docker stack deploy -c \"$DOCKER_COMPOSE_FILENAME\" --prune \"$DOCKER_COMPOSE_PREFIX\""
