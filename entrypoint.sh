ede4d146330b2ebd6f6665d2616b5b44e77f01cf
nuclearpolygon <45406953+nuclearpolygon@users.noreply.github.com>
2025-07-26 14:10:40 +0300

    fix recreate process

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,25 +14,22 @@ cleanup() {
 }
 trap cleanup EXIT
 
-log "Packing workspace into archive to transfer onto remote machine."
-tar cjvf /tmp/workspace.tar.bz2 --exclude .git .
-
+log "DOCKER_COMPOSE_FILENAME: $DOCKER_COMPOSE_FILENAME"
+log "DOCKER_COMPOSE_PREFIX: $DOCKER_COMPOSE_PREFIX"
+log "HOME: $HOME"
+ls -l .
 log "Launching ssh agent."
+
 eval `ssh-agent -s`
-
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace\" -xjv ; log DOCKER_COMPOSE_FILENAME: \"$DOCKER_COMPOSE_FILENAME\" ; log HOME: \"\$HOME/.workspace\" ; log DOCKER_COMPOSE_PREFIX: \"$DOCKER_COMPOSE_PREFIX\" ; log 'Launching docker compose...' ; cd \"\$HOME/.workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" down --rmi all ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --build --force-recreate"
-if $PULL; then
-  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...' ; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log DOCKER_COMPOSE_PREFIX \"$DOCKER_COMPOSE_PREFIX\" ; log HOME \"$HOME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace\" -xjv ; log 'Launching docker compose...' ; log DOCKER_COMPOSE_FILENAME: \"$DOCKER_COMPOSE_FILENAME\" ; log HOME: \"\$HOME/.workspace\" ; log DOCKER_COMPOSE_PREFIX: \"$DOCKER_COMPOSE_PREFIX\" ; cd \"\$HOME/.workspace\" ; log 'Pull images...'  ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" down --rmi all ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" pull ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --build --force-recreate" ;
-fi
-if $USE_DOCKER_STACK ; then
-  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/.workspace\" ; } ; log DOCKER_COMPOSE_FILENAME \"$DOCKER_COMPOSE_FILENAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/.workspace/$DOCKER_COMPOSE_PREFIX\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/.workspace/$DOCKER_COMPOSE_PREFIX\" -xjv ; log 'Launching docker stack deploy...' ; cd \"\$HOME/.workspace/$DOCKER_COMPOSE_PREFIX\" ; docker stack deploy -c \"$DOCKER_COMPOSE_FILENAME\" --prune \"$DOCKER_COMPOSE_PREFIX\""
-fi
-
-#
 ssh-add <(echo "$SSH_PRIVATE_KEY")
 
-echo ">> [local] Connecting to remote host."
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
-  "$SSH_USER@$SSH_HOST" -p "$SSH_PORT" \
-  "$remote_command" \
-  < /tmp/workspace.tar.bz2
+log "Creating docker context"
+docker context create --docker host=ssh://$SSH_USER@$SSH_HOST ssh-docker
+docker context use ssh-docker
+
+log "Launching docker compose"
+docker compose -f "$DOCKER_COMPOSE_FILENAME" -p "$DOCKER_COMPOSE_PREFIX" down --rmi all
+if $PULL; then
+  docker compose -f "$DOCKER_COMPOSE_FILENAME" -p "$DOCKER_COMPOSE_PREFIX" pull
+fi
+docker compose -f "$DOCKER_COMPOSE_FILENAME" -p "$DOCKER_COMPOSE_PREFIX" up -d --build --force-recreate
